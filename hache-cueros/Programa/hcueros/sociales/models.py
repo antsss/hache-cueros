fc4271ed5ebeff77361df8eeb1d45784f7df2a03
pbruno <paolaedithbruno@gmail.com>
2014-11-09 16:47:56 -0300

    Trabajando en la aplicacion Sociales

--- hache-cueros/Programa/hcueros/sociales/models.py
+++ hache-cueros/Programa/hcueros/sociales/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Social(models.Model):
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    tipo_social = models.CharField(max_length=1)
+    def __unicode__(self):              
+        return self.texto

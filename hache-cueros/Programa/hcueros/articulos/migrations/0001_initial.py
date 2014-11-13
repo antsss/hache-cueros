67201f8385755cb0428dc757b37f987bed3ee3ab
augustooscar <bustosaugusto62@gmail.com.ar>
2014-11-12 21:30:07 -0300

    ArticulosModificado

---
+++ hache-cueros/Programa/hcueros/articulos/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Articulo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(max_length=200)),
+                ('descripcion', models.CharField(max_length=500)),
+                ('publicado', models.BooleanField(default=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- hache-cueros/Programa/hcueros/articulos/models.py
+++ hache-cueros/Programa/hcueros/articulos/models.py
@@ -1,3 +1,25 @@
+#-*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+class Articulo(models.Model):
+	nombre = models.CharField(max_length=200)
+	descripcion = models.CharField(max_length=500)
+	publicado = models.BooleanField(default=False)
+	precio_ma = models.FloatField(null =True, blank = True)#uno es para el campo y otro para formulario
+	precio_mi = models.FloatField(null = True, blank = True)
+	publicado_ma = models.BooleanField(default=False)
+	publicado_mi = models.BooleanField(default=False)
+
+
+	def __unicode__(self):
+		return self.nombre #pongo que quiero que se muestre
+   
+class Categoria(models.Model):
+	nombre = models.CharField(max_length=200)
+	descripcion = models.CharField(max_length=500)
+	publicada = models.BooleanField(default=False)
+	articulos = models.ManyToManyField(Articulo)
+
+	def __unicode__(self):
+		return self.nombre #pongo que quiero que se muestre

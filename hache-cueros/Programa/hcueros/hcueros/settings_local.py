ed8b395e1fbe0dc5e7ca63f3c5f1f533f5eeea5c
augustooscar <bustosaugusto62@gmail.com>
2014-11-16 15:35:31 -0300

    borramos el setting_local que habiamos hecho localmente

--- hache-cueros/Programa/hcueros/hcueros/settings_local.py
+++
@@ -1,5 +0,0 @@
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-#ALLOWED_HOSTS = ['ww2.anayluistango.com.ar', 'www.anayluistango.com.ar',
-#'anayluistango.com.ar', '127.0.0.1', 'localhost']

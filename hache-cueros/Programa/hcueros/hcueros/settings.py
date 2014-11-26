b910150a291dbfa206a67ce375b547b507252e8d
pbruno <paolaedithbruno@gmail.com>
2014-11-26 18:56:04 -0300

    Arreglando sintaxis

--- hache-cueros/Programa/hcueros/hcueros/settings.py
+++ hache-cueros/Programa/hcueros/hcueros/settings.py
@@ -24,7 +24,7 @@ DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ckeditor',
-    'imagekit'
+    'imagekit',
     'sociales',
     'articulos',
 )
